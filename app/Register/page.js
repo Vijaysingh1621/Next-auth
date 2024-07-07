ab2eeaac8a9d0c5a68003a366196773c86d5aa55
Vijay Singh <rockcodingrookie21@gmail.com>
2024-07-08 00:52:00 +0530

    page_updated

--- app/Register/page.js
+++ app/Register/page.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { toast } from "react-toastify";
+import { useUrl } from 'next-url';
 
 import {
     Card,
@@ -39,9 +40,12 @@ const Register = () => {
         return;
       }
 
+            
+      const url = useUrl();
+      const registerUrl = new URL("/api/registerapi", url.origin);
+    
       try {
-        const url = new URL("/api/registerapi", window.location.origin);
-        const res = await fetch(url, {
+        const res = await fetch(registerUrl, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
